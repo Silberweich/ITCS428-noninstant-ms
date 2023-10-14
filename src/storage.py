b7e4027b98feae80303761160cbeadcc8b11b712
Silberweich <Snokitsune@gmail.com>
2023-10-14 11:27:51 +0700

    storage file done?

--- src/storage.py
+++ src/storage.py
@@ -8,42 +8,62 @@ class StorageSolution():
         self.path = path / "store.txt"
         self.file = open(self.path, "a+")
         self.msgLine = []
+        self.__loadMsgLine()
 
     def closeFileHandler(self, user:str) -> None:
         self.file.close()
 
     def storeMsg(self, msg: Message) -> bool:
-        self.file.write(messageToJSON(msg))
+        self.file.write(messageToJSON(msg).rstrip() + "\n")
         return True
     
-    def loadMsgLine(self) -> None:
-        print(self.file.read())
-        # for line in self.file.read():
-        #     print(">", line)
-        # self.msgLine = [messageFromJSON(line.rstrip()) for line in self.file.readlines()]
+    def __loadMsgLine(self) -> None:
+        self.file.seek(0)
+        self.msgLine = [messageFromJSON(line.rstrip()) for line in self.file.readlines()]
+        return None
+    
+    def __updateAllMsg(self) -> None:
+        self.file.seek(0)
+        self.file.truncate()
+
+        for msg in self.msgLine:
+            self.file.write(messageToJSON(msg).rstrip() + "\n")
+        
         return None
 
     def getMsgByHash(self, user:str, hash:str) -> Message:
-        return "hash"
+        self.__loadMsgLine()
+        for msg in self.msgLine:
+            if msg.toUsr == user and msg.msgHash == hash:
+                msg.incRequested()
+                self.__updateAllMsg()
+                return msg
+        return None
     
     def getNewMsg(self, user:str) -> List[Message]:
-        return "hash"
+        self.__loadMsgLine()
+        messages = []
+        for msg in self.msgLine:
+            if msg.toUsr == user and msg.timesRequested == 0:
+                messages.append(msg)
+                msg.incRequested()
+        self.__updateAllMsg()
+        return messages
     
     def getAllMsg(self, user:str) -> List[Message]:
-        return "hash"
+        self.__loadMsgLine()
+        messages = []
+        for msg in self.msgLine:
+            if msg.toUsr == user:
+                messages.append(msg)
+                msg.incRequested()
+        self.__updateAllMsg()
+        return messages
     
 if __name__ == "__main__":
     store = StorageSolution(Path.cwd())
-    print("test")
-    msg = createMessage("from", "to", "datadatadatadatadata")
-    store.storeMsg(msg)
-    msg = createMessage("test", "testrecv", "better answer my message soon")
-    store.storeMsg(msg)
-    msg = createMessage("test", "testrecv", "Ey yo, answer my message")
-    store.storeMsg(msg)
-    msg = createMessage("from", "to", "dqaaaaaa")
-    store.storeMsg(msg)
-    msg = createMessage("from", "to", "dataddsdsddsdsd")
-    store.storeMsg(msg)
-    store.loadMsgLine()
+    
     print(store.msgLine)
+    # print("\n\n>", store.getNewMsg("testrecv"))
+    print("\n\n>", store.getAllMsg("testrecv"))
+    print("\n\n>", store.getMsgByHash("to","fe7f20fc7974ef4b8aa9727966133ba5"))
